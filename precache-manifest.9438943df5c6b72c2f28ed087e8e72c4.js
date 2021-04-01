bfc514e13da7f64075dc13fd2e88f89272f3ab05
Code Sandbox <hello@codesandbox.io>
2021-04-01 23:29:19 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9438943df5c6b72c2f28ed087e8e72c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4cec355ea1b900c3f37372b258d9d4",
+    "url": "https://ricobuttonpusher.github.io/csb-fd28j/index.html"
+  },
+  {
+    "revision": "97d7a9da7f6045e713a4",
+    "url": "https://ricobuttonpusher.github.io/csb-fd28j/static/css/main.27ddcc72.chunk.css"
+  },
+  {
+    "revision": "63d3a5c02fad72b76c3b",
+    "url": "https://ricobuttonpusher.github.io/csb-fd28j/static/js/2.b9bfeda9.chunk.js"
+  },
+  {
+    "revision": "97d7a9da7f6045e713a4",
+    "url": "https://ricobuttonpusher.github.io/csb-fd28j/static/js/main.b28638e5.chunk.js"
+  },
+  {
+    "revision": "8c3ac856ada67ed7dbe3",
+    "url": "https://ricobuttonpusher.github.io/csb-fd28j/static/js/runtime~main.7730e4c5.js"
+  }
+]);
